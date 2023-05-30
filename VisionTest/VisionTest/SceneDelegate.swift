cf4a801e0bcad38b6c354186f33e3d7827d59f8e
wnsxor1993 <https://wnsxor1993@github.com>
2023-05-30 13:32:23 +0900

    chore: 프로젝트 생성

---
+++ VisionTest/VisionTest/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  VisionTest
+//
+//  Created by Zeto on 2023/05/30.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ VisionTest/VisionTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  VisionTest
+//
+//  Created by Zeto on 2023/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
