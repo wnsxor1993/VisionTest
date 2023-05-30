f1debbaa2646aeae070770c5981a1fb2944dd381
wnsxor1993 <https://wnsxor1993@github.com>
2023-05-30 15:51:21 +0900

    feat: Vision 구현

--- VisionTest/VisionTest/ViewController.swift
+++ VisionTest/VisionTest/ViewController.swift
@@ -13,14 +13,39 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
     
+    private var vnTextRequest: VNRecognizeTextRequest?
+    private var vnRequestHandlers: [VNImageRequestHandler] = []
+    
     private var textImages: [CGImage] = []
+    private var convertedTexts: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configureVision()
+    }
+    
+    @IBAction func executeVisionOCR(_ sender: UIButton) {
+        do {
+            guard let vnTextRequest else { return }
+            
+            try self.vnRequestHandlers.forEach {
+                try $0.perform([vnTextRequest])
+            }
+            
+        } catch {
+            print("Execute Error: \(error.localizedDescription)")
+        }
     }
 }
 
 private extension ViewController {
+    
+    func configureVision() {
+        self.configureTextImages()
+        self.configureVisionRequest()
+        self.configureVisionSetting()
+    }
     
     func configureTextImages() {
         for num in 1...5 {
@@ -32,7 +57,45 @@ private extension ViewController {
         }
     }
     
-    func configureVision() {
+    func configureVisionRequest() {
         guard !(textImages.isEmpty) else { return }
+        
+        textImages.forEach {
+            let vnRequestHandler: VNImageRequestHandler = .init(cgImage: $0)
+            self.vnRequestHandlers.append(vnRequestHandler)
+        }
+        
+        let request: VNRecognizeTextRequest = .init { [weak self] request, error in
+            guard let self, let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            
+            if let error {
+                print("Request Error: \(error.localizedDescription)")
+                
+                return
+            }
+            
+            let text = observations.compactMap {
+                $0.topCandidates(1).first?.string
+            }.joined(separator: "\n")
+            
+            if convertedTexts.isEmpty {
+                self.convertedTexts = text
+            } else {
+                self.convertedTexts += "\n\n\(text)"
+            }
+        }
+        
+        self.vnTextRequest = request
+    }
+    
+    func configureVisionSetting() {
+        if #available(iOS 16.0, *) {
+            let revision3: Int = VNRecognizeTextRequestRevision3
+            self.vnTextRequest?.revision = revision3
+        }
+        
+        self.vnTextRequest?.recognitionLevel = .accurate
+        self.vnTextRequest?.recognitionLanguages = ["ko-KR"]
+        self.vnTextRequest?.usesLanguageCorrection = true
     }
 }
