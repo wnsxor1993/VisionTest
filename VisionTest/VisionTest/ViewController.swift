da80519dd34a5eedbdd23cc0fe3e5e095197b08d
wnsxor1993 <https://wnsxor1993@github.com>
2023-05-30 14:40:49 +0900

    feat: TextImage 추가 및 변환

--- VisionTest/VisionTest/ViewController.swift
+++ VisionTest/VisionTest/ViewController.swift
@@ -6,11 +6,33 @@
 //
 
 import UIKit
+import VisionKit
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    private var textImages: [CGImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+private extension ViewController {
+    
+    func configureTextImages() {
+        for num in 1...5 {
+            let image: UIImage? = .init(named: "\(num)")
+            
+            guard let image,  let cgImage: CGImage = image.cgImage else { continue }
+            
+            self.textImages.append(cgImage)
+        }
+    }
+    
+    func configureVision() {
+        guard !(textImages.isEmpty) else { return }
+    }
+}
