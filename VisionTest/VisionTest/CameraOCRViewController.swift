6a6b32b7e034de33d0b4d61793eae6dd2d9672c1
wnsxor1993 <https://wnsxor1993@github.com>
2023-05-31 11:05:45 +0900

    feat: Camera용 VC 추가

---
+++ VisionTest/VisionTest/CameraOCRViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CameraOCRViewController.swift
+//  VisionTest
+//
+//  Created by Zeto on 2023/05/31.
+//
+
+import UIKit
+
+final class CameraOCRViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
