d076b19b96e53d91fdbc94cba16f06dd5893fd00
wnsxor1993 <https://wnsxor1993@github.com>
2023-05-31 13:02:06 +0900

    feat: VNDocumentCamera 구현

--- VisionTest/VisionTest/CameraOCRViewController.swift
+++ VisionTest/VisionTest/CameraOCRViewController.swift
@@ -6,10 +6,49 @@
 //
 
 import UIKit
+import VisionKit
+import Vision
 
 final class CameraOCRViewController: UIViewController {
     
+    @IBOutlet weak var textView: UITextView!
+    
+    private let vnCameraVC: VNDocumentCameraViewController = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func takePicture(_ sender: UIButton) {
+        self.present(vnCameraVC, animated: true)
+    }
+}
+
+extension CameraOCRViewController: VNDocumentCameraViewControllerDelegate {
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        for pageNumber in 0..<scan.pageCount {
+            let image = scan.imageOfPage(at: pageNumber)
+        }
+
+        controller.dismiss(animated: true)
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+        print("CameraDocumentError: \(error.localizedDescription)")
+        
+        controller.dismiss(animated: true)
+    }
+    
+    func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        controller.dismiss(animated: true)
+    }
+}
+
+private extension CameraOCRViewController {
+    
+    func configureVision() {
+        self.vnCameraVC.delegate = self
+        self.vnCameraVC.modalPresentationStyle = .fullScreen
     }
 }
